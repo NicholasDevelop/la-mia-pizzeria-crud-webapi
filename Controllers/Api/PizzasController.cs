c416a95e583c6783d36cbc627ef4dd627893cdd0
NicholasDevelop <marcaccini.n@gmail.com>
2022-07-26 12:18:07 +0200

    Creata rotta GET per le API delle pizze

---
+++ Controllers/Api/PizzasController.cs
@@ -0,0 +1,23 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+    [Route("api/pizzas")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using(PizzeriaContext context = new PizzeriaContext())
+            {
+                List<Pizza> pizzaList = context.Pizzas.ToList();
+                //IQueryable<Pizza> pizzaList = (IQueryable<Pizza>)ctx.Pizzas.ToList<Pizza>();
+                return Ok(pizzaList);
+            }
+        }
+    }
+}
