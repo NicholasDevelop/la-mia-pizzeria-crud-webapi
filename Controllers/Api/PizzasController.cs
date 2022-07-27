615a32dbacc6e8606459fd1d5d9ce8b598c84e20
NicholasDevelop <marcaccini.n@gmail.com>
2022-07-27 12:57:39 +0200

    Provato ad implementare la search delle pizze... ancora da vedere

--- Controllers/Api/PizzasController.cs
+++ Controllers/Api/PizzasController.cs
@@ -6,18 +6,39 @@ using Microsoft.EntityFrameworkCore;
 
 namespace la_mia_pizzeria_static.Controllers.Api
 {
-    [Route("api/pizzas")]
+    [Route("api/[controller]")]
     [ApiController]
     public class PizzasController : ControllerBase
     {
         [HttpGet]
         //[Route("Get")]
-        public IActionResult Get()
+        public IActionResult Get(string? search)
         {
             using(PizzeriaContext context = new PizzeriaContext())
             {
-                List<Pizza> pizzaList = context.Pizzas.ToList();
-                return Ok(pizzaList);
+                IQueryable<Pizza> pizzas = context.Pizzas;
+
+                if(search != null && search != ""){
+                    pizzas = pizzas.Where(p => p.Name.Contains(search));
+                }
+
+                return Ok(pizzas.ToList());
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            using (PizzeriaContext context = new PizzeriaContext())
+            {
+                Pizza p = context.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+
+                if(p == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(p);
             }
         }
     }
