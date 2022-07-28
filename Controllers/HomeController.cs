a16f345e18615414e28e8d5d559fa92605acc43e
NicholasDevelop <marcaccini.n@gmail.com>
2022-07-28 17:14:45 +0200

    Creato form funzionante

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace la_mia_pizzeria_static.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-
+        
         public IActionResult Contact()
         {
             return View();
